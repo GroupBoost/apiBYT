68badf6aadcae9b3f240e560f074d248c836dc6f
waxa <waxa@skynet>
2015-02-27 23:02:50 +0100

    estructura

---
+++ prueba.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import web
+import json
+
+urls = (
+	'/(.*)', 'basura'
+)
+
+app = web.application(urls, globals())
+
+class basura:
+	def GET(self, basura):
+		return "<h1>Nop</h1>"
+
+if __name__ == "__main__":
+	app.run()
